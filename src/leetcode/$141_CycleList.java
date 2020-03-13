fd8136f1e6f8cb6657b21df24d933421a9d66e30
deecyn <deecyns@gmail.com>
2020-03-13 22:35:48 +0800

    update

--- src/leetcode/$141_CycleList.java
+++ src/leetcode/$141_CycleList.java
@@ -26,7 +26,10 @@ public class $141_CycleList {
 
     /**
      * 通过检查一个结点此前是否被访问过来判断链表是否为环形链表，
-     * 使用哈希表来存储之前的遍历结果。
+     * 使用哈希表来存储之前的遍历结果。时间复杂度 O(n)。
+     *
+     * 但使用哈希表时，new 对象与映射寻址也需要的时间，虽然时间量级为 O(1)，
+     * 但实际时间比 O(1) 大一些。所以，使用哈希表会比双指针的解法，实际耗费时间多一些。
      */
     public boolean byHashSet(ListNode head){
         Set<ListNode> visitedSet = new HashSet<>();
@@ -44,7 +47,8 @@ public class $141_CycleList {
     /**
      * 使用快、慢双指针遍历：
      * 若链表中不存在环，则快指针直接先到达链表尾部，此时可以返回 false；
-     * 若链表中存在环，则快指针不停地在环上遍历，最终与慢指针相遇，此时可以返回 true
+     * 若链表中存在环，则快指针不停地在环上遍历，最终与慢指针相遇，此时可以返回 true。
+     * 时间复杂度 O(n)
      */
     public boolean byDoublePointer(ListNode head){
         if (head == null || head.next == null){
--- src/leetcode/$70_ClimbStairs.java
+++ src/leetcode/$70_ClimbStairs.java
@@ -10,14 +10,14 @@ public class $70_ClimbStairs {
 //        int[] memo = new int[n + 1];
 //        return funD(0, n, memo);
 
-        return funA(n);
+        return byIterationVar(n);
     }
 
     /**
      * 在循环中，向后累加时，只有前两项有用，采用 3 个变量存储实时数据；
      * 时间复杂度 O(n)，空间复杂度 O(1)
      */
-    public int funA(int n){
+    public int byIterationVar(int n){
         if (n <= 2) return n;
 
         int f1 = 1, f2 = 2, f3 = 3;
@@ -32,7 +32,7 @@ public class $70_ClimbStairs {
     /**
      * 使用循环代替递归，时间复杂度 O(n)，空间复杂度 O(n)
      */
-    public int funB(int n){
+    public int byIteration(int n){
         if (n <= 2) return n;
 
         int[] num = new int[n+1];
@@ -48,7 +48,7 @@ public class $70_ClimbStairs {
      * n 阶楼梯方法数：f(n) = f(n-1) + f(n-2)
      * 采用递归，时间复杂度 O(2^n)，容易超时
      */
-    public int funC(int i, int n){
+    public int byRecursion(int i, int n){
         if (i > n){
             return 0;
         }
@@ -56,13 +56,13 @@ public class $70_ClimbStairs {
         if (i == n){
             return 1;
         }
-        return funC(i + 1, n) + funC(i + 2, n);
+        return byRecursion(i + 1, n) + byRecursion(i + 2, n);
     }
 
     /**
      * 记忆化递归，优化冗余的计算，时间复杂度 O(n)
      */
-    public int funD(int i, int n, int[] memo){
+    public int byMemoryRecursion(int i, int n, int[] memo){
         if (i > n){
             return 0;
         }
@@ -74,6 +74,6 @@ public class $70_ClimbStairs {
         if (memo[i] > 0){
             return memo[i];
         }
-        return memo[i] = funD(i + 1, n, memo) + funD(i + 2, n, memo);
+        return memo[i] = byMemoryRecursion(i + 1, n, memo) + byMemoryRecursion(i + 2, n, memo);
     }
 }
