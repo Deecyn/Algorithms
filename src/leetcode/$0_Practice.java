8a752d2d64d4fbb5bd115411de48e08ec19101e5
deecyn <deecyns@gmail.com>
2020-03-15 22:23:50 +0800

    update

--- src/leetcode/$0_Practice.java
+++ src/leetcode/$0_Practice.java
@@ -9,4 +9,67 @@ import java.util.*;
  */
 public class $0_Practice {
 
+    private static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+
+    public int byIterationVar(int n){
+        if (n < 3) return n;
+
+        int f1 = 1, f2 = 2, f3 = 3;
+        for (int i = 3; i <= n; i++) {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+
+    public int byIteration(int n){
+        if (n < 3) return n;
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+
+    public int byRecursion(int i, int n){
+        if (i > n){
+            return 0;
+        }
+        if (i == n){
+            return 1;
+        }
+
+        return byRecursion(i + 1, n) + byRecursion(i + 2, n);
+    }
+
+    public int byMemoryRecursion(int i, int n, int[] memo){
+        if (i > n){
+            return 0;
+        }
+        if (i == n){
+            return 1;
+        }
+
+        if (memo[i] > 0){
+            return memo[i];
+        }
+
+        return memo[i] = byMemoryRecursion(i + 1, n, memo) + byMemoryRecursion(i + 2, n, memo);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/leetcode/$141_CycleList.java
+++ src/leetcode/$141_CycleList.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class $141_CycleList {
 
 
-    class ListNode {
+    private class ListNode {
       int val;
       ListNode next;
       ListNode(int x) {
